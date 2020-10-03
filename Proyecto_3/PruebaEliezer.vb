39ab7e6726b32045fe9c22e867eb29de2a43b1ba
EliezerDLC <70461580+EliezerDLC@users.noreply.github.com>
2020-10-02 18:53:19 -0600

    Proyecto funcional

--- Proyecto_3/PruebaEliezer.vb
+++ Proyecto_3/PruebaEliezer.vb
@@ -1,3 +1,9 @@
 ﻿Public Class PruebaEliezer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("Pinche bato pendejo")
+    End Sub
 
+    Private Sub PruebaEliezer_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
